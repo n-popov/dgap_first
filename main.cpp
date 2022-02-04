f8be869332e0561240cbcac5a6671b50b0e5c914
Nikita <popov.nv@phystech.edu>
2022-02-04 11:36:28 +0300

    timer is here!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <chrono>
+#include <cmath>
+
+using namespace std::chrono;
+
+class Timer {
+public:
+    Timer() {}
+    ~Timer() {
+        std::cout <<
+            duration_cast<std::chrono::microseconds>
+                    (steady_clock::now() - start).count()
+                    << std::endl;
+    }
+
+private:
+    steady_clock::time_point start = steady_clock::now();
+};
 
 int main() {
-    // Here we saying Hello to DGAP
-    std::cout << "Hello, DGAP!" << std::endl;
+    double result = 0.;
+    {
+        Timer t;
+        for(auto i = 0u; i < 10000000u; i++) {
+            result += std::sin(i) + std::cos(i);
+        }
+    }
+    std::cout << "Result is " << result;
     return 0;
 }
